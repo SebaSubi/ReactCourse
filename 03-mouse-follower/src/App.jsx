5673af61b1cfb7a8f0df6e6102912c19defd0381
SebaSubi <sebaperezlavooy@gmail.com>
2024-04-14 15:01:46 -0300

    Completed mouse follower project

--- 03-mouse-follower/src/App.jsx
+++ 03-mouse-follower/src/App.jsx
@@ -1,9 +1,59 @@
+import { useEffect, useState } from 'react'
 
 import './App.css'
 
 function App() {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({x: 0, y: 0})
+  useEffect(() => {
+    // console.log('Efect')
+    const handleMove = (event) => {
+      const {clientX, clientY} = event
+      setPosition({x: clientX, y: clientY} )
+      
+    }
+
+    if(enabled){
+      window.addEventListener('pointermove', handleMove) //the eventlistener 'pointermove' gives you the x and y coordinates of the mouse
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove) //We remove the event listener evey time enable changes so it,
+      //dosent continue to follow the mouse even when the button is in its deactivated state
+      //With the command getEventListeners(window(for example)), you can return all the events that you are subscribed to,
+      //this is very handy to debug, it only woeks in chronium tho
+
+      setPosition({x: 0, y: 0 }) //Reset the position of the mouse follower so it dosent say over the button
+    } 
+    // --> You can add "Cleaners" to the useEffect.
+    // --> This return executes every time ether the component stopr rendering, OR when the dependency changes <-- this is what we want
+    // --> An example of the first case would be if all this logic would be in a seperate component, and the App() would just
+    //render that component. Here, every time the App would stop rendering that component, this return would execute.
+    // --> This is a very good practice, if we dirint do this, every time th ebutton is presed, a new event listener would be created, 
+    //and it would start inporperly working and lagging
+   
+  }, [enabled]) //Never add a useEffect, inside a conditional
+
   return (
-    <h3>Proyecto 3</h3>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }
+      }/>
+      <h3>Proyecto 3</h3>
+      <button onClick={() => (setEnabled(!enabled))}>
+        {enabled ? 'Deactivate' : 'Activate'} mouse follower
+      </button>
+    </main>
   )
 }
 export default App
--- 03-mouse-follower/src/main.jsx
+++ 03-mouse-follower/src/main.jsx
@@ -4,7 +4,10 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StrictMode> 
     <App />
   </React.StrictMode>,
-)
+)// -->The <React.StrictMode> is the reason why useEffect executes, cleans up, and executes.
+// --> <React.StrictMode> does many things, tells you if your using old react code, if you are doing something incorrectly, etc.
+//AND every time a component is mounted, it runds the effect, cleans it up, and runs it again, why? to make sure that your 
+//component and effect are working properly
